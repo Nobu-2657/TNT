b652957ab1cb5a7e10f415fcdc64a8e6e650321e
Nobu-2657 <221205009@ccmailg.meijo-u.ac.jp>
2023-11-29 16:30:56 +0900

    ポップアップ画面の設定

--- app/src/main/java/pbl/tnt/MainActivity.java
+++ app/src/main/java/pbl/tnt/MainActivity.java
@@ -3,12 +3,22 @@ package pbl.tnt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Snackbar.make(view, "ボタン１が押されました", Snackbar.LENGTH_SHORT).show();
     }
 }
